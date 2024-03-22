b35f97bb52a63ce7751e69b3276558cec65539f0
Soma Sekine <a8121151@aoyama.jp>
2024-03-22 15:11:46 -0400

    写真追加、ヘッダーフッター微調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
-
+import Image from "next/image";
+import homeImage from "../../src/public/home_pic.jpg";
 export default function Index() {
   const allPosts = getAllPosts();
 
@@ -13,25 +14,12 @@ export default function Index() {
 
   return (
     <main>
-      <Container>
-      <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-xl">Somahc</span>
-          </a>
-          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            <a className="mr-5 hover:text-gray-900">About</a>
-            <a className="mr-5 hover:text-gray-900">Blog</a>
-          </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
-        </div>
-      </header>
-
-
+      
+        <Image
+          className="px-5"
+          src={homeImage}
+          alt="ホーム写真"
+        />
         {/* <Intro /> */}
         {/* <HeroPost
           title={heroPost.title}
@@ -42,22 +30,6 @@ export default function Index() {
           excerpt={heroPost.excerpt}
         />
         {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
-        <footer className="text-gray-600 body-font">
-          <div className="bg-gray-100">
-            <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-              <p className="text-gray-500 text-sm text-center sm:text-left">© 2024 Somahc
-              </p>
-              <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                <a className="ml-3 text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-              </span>
-            </div>
-          </div>
-        </footer>
-      </Container>
     </main>
   );
 }
