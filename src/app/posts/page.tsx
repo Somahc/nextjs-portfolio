4ec2668c4828ef4b48b58f0d45cafe431cbb10c3
Soma Sekine <a8121151@aoyama.jp>
2024-03-23 17:53:23 -0400

    ブログ一覧ページほぼ完成

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,14 @@
+import { getAllPosts } from "@/lib/api";
+import { MoreStories } from "../_components/more-stories";
+import { PostsList } from "../_components/postsList";
+
+export default function Posts() {
+    const allPosts = getAllPosts();
+
+    return (
+        <>
+            {/* <MoreStories posts={allPosts} /> */}
+            <PostsList posts={allPosts} />
+        </>
+    )
+}
